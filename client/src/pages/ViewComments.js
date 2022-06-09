b93d39682144b736889415ff0046650309ee6928
Mariana Tapia <mglopez2113@gmail.com>
2022-06-09 14:12:38 -0700

    updated client routes

---
+++ client/src/pages/ViewComments.js
@@ -0,0 +1,4 @@
+function ViewComments() {
+  return <h2> ViewComments Page </h2>;
+}
+export default ViewComments;
--- client/src/util/queries.js
+++ client/src/util/queries.js
@@ -1,5 +1,4 @@
 import { gql } from "@apollo/client";
-
 export const ME = gql`
   query me {
     me {
@@ -10,3 +9,45 @@ export const ME = gql`
     }
   }
 `;
+
+// commented out may be able to use for later queries need to work on pages first
+
+// export const FIND_USER = gql`
+// query FindUser($userId: ID!) {
+//   findUser(userId: $userId) {
+//     _id
+//     username
+//     email
+//     post {
+//       _id
+//       author
+//       createdAt
+//       postText
+//     }
+//   }
+// }`;
+
+// export const QUERY_USER = gql`
+// query Users{
+//   users {
+//     _id
+//     username
+//     email
+//     lastLogin
+//     age
+//     city
+//     skinType
+//     cleanser
+//     toner
+//     moisturizer
+//     serums
+//     masks
+//     post{
+//       _id
+//       author
+//       createdAt
+//       postText
+//     }
+//   }
+// }
+// `;
