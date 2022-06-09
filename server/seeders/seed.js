030a629150f709c2bdc68ca362d74778424e3f2a
Cody Polland <cody.polland@gmail.com>
2022-06-09 14:27:22 -0700

    seeded data added for testing

---
+++ server/seeders/seed.js
@@ -0,0 +1,31 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../", ".env") });
+const db = require("../config/connection");
+const { User } = require("../models");
+const userSeeds = require("./userSeeds.json");
+
+db.once("open", async () => {
+  try {
+    //   await User.({});
+
+    await User.create(userSeeds);
+
+    //   for (let i = 0; i < thoughtSeeds.length; i++) {
+    //     const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    //     const user = await User.findOneAndUpdate(
+    //       { username: thoughtAuthor },
+    //       {
+    //         $addToSet: {
+    //           thoughts: _id,
+    //         },
+    //       }
+    //     );
+    //   }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
