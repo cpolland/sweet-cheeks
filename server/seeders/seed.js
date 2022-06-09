f36abceed51478e10e8c795bf79b5bbe9c61b679
Cody Polland <cody.polland@gmail.com>
2022-06-09 14:37:04 -0700

    seeded data with client updated

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -6,21 +6,7 @@ const userSeeds = require("./userSeeds.json");
 
 db.once("open", async () => {
   try {
-    //   await User.({});
-
     await User.create(userSeeds);
-
-    //   for (let i = 0; i < thoughtSeeds.length; i++) {
-    //     const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-    //     const user = await User.findOneAndUpdate(
-    //       { username: thoughtAuthor },
-    //       {
-    //         $addToSet: {
-    //           thoughts: _id,
-    //         },
-    //       }
-    //     );
-    //   }
   } catch (err) {
     console.error(err);
     process.exit(1);
