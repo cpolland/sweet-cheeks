afd96edfac15e27b2b9ee7fa89379cd2107630bc
Fiona Carroll <fiona.carroll@me.com>
2022-06-10 16:20:12 -0700

    initial browse profiles page commit

--- client/src/pages/BrowseProfiles.js
+++ client/src/pages/BrowseProfiles.js
@@ -1,4 +1,34 @@
-function BrowseProfile() {
-  return <h2> Browse Profile Page </h2>;
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../util/queries";
+
+function BrowseProfiles() {
+  const params = useParams();
+  console.log(params);
+
+  const { loading, error, data } = useQuery(QUERY_USER, {
+    variables: params,
+  });
+  console.log(loading);
+  console.log(data);
+  if (error) {
+    console.log(error);
+  }
+  return (
+    <>
+      {loading ? (
+        <> "loading" </>
+      ) : (
+        <div>
+          <h1>{data.email}</h1>
+          {/* <h3>{data.post.description}</h3> */}
+        </div>
+      )}
+    </>
+  );
+
+  // function BrowseProfile() {
+  //   return <h2> Browse Users </h2>;
+  // }
 }
-export default BrowseProfile;
+export default BrowseProfiles;
