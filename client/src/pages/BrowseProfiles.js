41a60d2dcb2c18ed9a171e5c3e002e3bc251e012
Fiona Carroll <fiona.carroll@me.com>
2022-06-10 16:35:42 -0700

    added browseprofile placeholder

--- client/src/pages/BrowseProfiles.js
+++ client/src/pages/BrowseProfiles.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../util/queries";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function BrowseProfiles() {
   const params = useParams();
@@ -14,17 +15,83 @@ function BrowseProfiles() {
   if (error) {
     console.log(error);
   }
+  const user = data?.findUser || {};
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <>
-      {loading ? (
-        <> "loading" </>
-      ) : (
-        <div>
-          <h1>{data.email}</h1>
-          {/* <h3>{data.post.description}</h3> */}
-        </div>
-      )}
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+
+              <Card.Text>Stephlovesskincare12</Card.Text>
+              <Card.Text>Age: 24</Card.Text>
+              <Card.Text>City: Denver</Card.Text>
+              <Card.Link href="#">View Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+
+              <Card.Text>Stephlovesskincare12</Card.Text>
+              <Card.Text>Age: 24</Card.Text>
+              <Card.Text>City: Denver</Card.Text>
+              <Card.Link href="#">View Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+
+              <Card.Text>Stephlovesskincare12</Card.Text>
+              <Card.Text>Age: 24</Card.Text>
+              <Card.Text>City: Denver</Card.Text>
+              <Card.Link href="#">View Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+
+              <Card.Text>Stephlovesskincare12</Card.Text>
+              <Card.Text>Age: 24</Card.Text>
+              <Card.Text>City: Denver</Card.Text>
+              <Card.Link href="#">View Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+
+              <Card.Text>Stephlovesskincare12</Card.Text>
+              <Card.Text>Age: 24</Card.Text>
+              <Card.Text>City: Denver</Card.Text>
+              <Card.Link href="#">View Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 
   // function BrowseProfile() {
