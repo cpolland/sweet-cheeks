8c6df4222048ce6fc746cc2d070b61325a9e5e9c
Fiona Carroll <fiona.carroll@me.com>
2022-06-10 12:04:28 -0700

    home.js react bootstrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useAuth } from "../util/auth";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
@@ -8,11 +9,17 @@ export default function Home() {
       <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
       <hr />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo cumque
-        explicabo ipsum, facilis repellendus omnis amet in accusantium quisquam
-        nam qui consectetur sunt distinctio nemo molestiae ratione. Iure,
-        aliquam debitis.
+        Here at Sweet-Cheeks we promote sisterhood and body positivity! It seems
+        these days skincare has dominated the internet. Our goal is to create a
+        community where everyone can share their skincare regimen and list
+        products a user suggests for their corresponding skin type. Sign up
+        today and see what other skincare regimens our community is using!
       </p>
+      <>
+        <Button variant="primary" size="lg" active>
+          Sign-Up
+        </Button>{" "}
+      </>
     </div>
   );
 }
