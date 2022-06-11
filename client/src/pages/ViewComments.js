a8d4e756e30edae7fcb16e244d18eb9a224fe5c3
lilynamou <lilyananamou@gmail.com>
2022-06-10 17:24:19 -0700

    Added Add comment page

--- client/src/pages/ViewComments.js
+++ client/src/pages/ViewComments.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import {  QUERY_POST } from '../util/queries';
 
-
+import { Card } from 'react-bootstrap';
 function ViewComments() {
   const params  = useParams();
   console.log(params);
@@ -24,10 +24,18 @@ return (
  {
  
  loading ? <> "loading" </>: 
-   <div>
-   <h1>{data.post}</h1>
-   {/* <h3>{data.post.description}</h3> */}
-   </div>
+  //  <div>
+  //  <h1>{data.post}</h1>
+  //  <h3>Yes, i love it!</h3>
+  //  </div>
+  <Card>
+  <Card.Header>Stephlovesskincare</Card.Header>
+  <Card.Body>
+    <Card.Text>
+      I love it!
+    </Card.Text>
+  </Card.Body>
+</Card>
  }
  </>
  )
