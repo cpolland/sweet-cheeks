b38b155064ba796f9e8ff6b513d2a5b00d648116
Mariana Tapia <mglopez2113@gmail.com>
2022-06-10 19:25:23 -0700

    individual profile placeholders added and rendering

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,39 +1,60 @@
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../util/queries';
-
+import { useParams } from "react-router-dom";
+import { from, useQuery } from "@apollo/client";
+import { QUERY_USER } from "../util/queries";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function Profile() {
-  const params  = useParams();
+  const params = useParams();
   console.log(params);
 
   const { loading, error, data } = useQuery(QUERY_USER, {
-    variables: params
+    variables: params,
   });
   console.log(loading);
   console.log(data);
-if(error) {
-  console.log(error);
+  if (error) {
+    console.log(error);
+  }
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
 
+              <Card.Text>PlovesTheMist</Card.Text>
+              <Card.Text>Age: 33</Card.Text>
+              <Card.Text>City: San Diego</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Skin Regime</Card.Title>
 
+              <Card.Text>
+                Skin type: Dry-sensitive Cleanser: Glossier Milk Wash Toner: The
+                Ordinary Toner Mosturizer/SPF: Glow Recipe: Plump creme Masks:
+                Holler & Glow eye Masks Serums:""
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
 
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>My Skin Bio</Card.Title>
 
-}
-return (
-  <>
- {
- 
- loading ? <> "loading" </>: 
-   <div>
-   <h1>{data.age}</h1>
-   {/* <h3>{data.post.description}</h3> */}
-   </div>
- }
- </>
- )
-
-
-
+              <Card.Text>
+                "Lorem ipsum.................................."
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Profile;
